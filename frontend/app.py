86fb5bffb7755939de23887c93eed8029764b581
Ivan Oteiza Q. <155277383+IvanOteizaQ@users.noreply.github.com>
2026-07-28 11:51:11 -0400

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -174,7 +174,7 @@ if 'vista_actual' not in st.session_state:
 banner_html = """
 <div style="
     background-color: #c41e3a;
-    padding: 3px 20px;
+    padding: 300px 20px;
     border-radius: 5px;
     margin-bottom: 20px;
     text-align: center;
